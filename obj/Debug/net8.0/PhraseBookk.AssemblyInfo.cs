f6453bc5d8a10d2938ee221a365cfbaacacdd597
AkhonaMaphumulo <akhonamaphumulo24@gmail.com>
2026-07-26 15:36:39 +0200

    Fix duplicate properties in PhraseSearchViewModel

--- obj/Debug/net8.0/PhraseBookk.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhraseBookk.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhraseBookk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee1a8f91bd454b000b8cafd1544fff14b39f1de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c7ba314b48adcdf861ad2ae75c79eb8da561c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhraseBookk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhraseBookk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
